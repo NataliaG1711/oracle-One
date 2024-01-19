bd9cc1d6cc6b5ecaba3f0c673fe2d8b703d853fd
NataliaG1711 <nataliagiraldoamador@gmail.com>
2024-01-19 16:49:27 -0500

    mensaje confirmar

---
+++ explorar funciones y listas/2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,9 @@
+let headline = document.querySelector("h1")
+/*es un método para acceder a los elementos del html, donde se le dice a la cantidad de elementos que hay
+donde se le pasa un nombre ( de etiqueta en este caso), donde se le atribuye a una variable (siendo un objeto) donde 
+se pueden realizar diferentes cosas
+*/
+headline.innerHTML = "Juego del número secreto"
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML ="elige un número del 1 al 10";
