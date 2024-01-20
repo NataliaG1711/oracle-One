882060f37237be9bc2fed4884f7b711b259b901a
NataliaG1711 <nataliagiraldoamador@gmail.com>
2024-01-20 18:49:41 -0500

    mensaje de confirmación

--- explorar funciones y listas/2035-logica-programacion-2-Aula1/app.js
+++ explorar funciones y listas/2035-logica-programacion-2-Aula1/app.js
@@ -2,8 +2,14 @@ let headline = document.querySelector("h1")
 /*es un método para acceder a los elementos del html, donde se le dice a la cantidad de elementos que hay
 donde se le pasa un nombre ( de etiqueta en este caso), donde se le atribuye a una variable (siendo un objeto) donde 
 se pueden realizar diferentes cosas
+
+Es parte del DOM: Document Object Model
 */
 headline.innerHTML = "Juego del número secreto"
 
-let parrafo = document.querySelector("p");
-parrafo.innerHTML ="elige un número del 1 al 10";
+let paragraph = document.querySelector("p");
+paragraph.innerHTML ="elige un número del 1 al 10";
+
+function userAttempt(){
+    
+}
