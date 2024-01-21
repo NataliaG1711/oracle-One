29cd15e76e8cf40fd39729e71f001142768fb13f
NataliaG1711 <nataliagiraldoamador@gmail.com>
2024-01-21 00:03:24 -0500

    confirmar

--- explorar funciones y listas/2035-logica-programacion-2-Aula1/app.js
+++ explorar funciones y listas/2035-logica-programacion-2-Aula1/app.js
@@ -1,15 +1,20 @@
-let headline = document.querySelector("h1")
+//Elemento Hoisting donde hace el elemento de las variables de las funciones al inicio para que queden disponibles
+
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento)
 /*es un método para acceder a los elementos del html, donde se le dice a la cantidad de elementos que hay
 donde se le pasa un nombre ( de etiqueta en este caso), donde se le atribuye a una variable (siendo un objeto) donde 
 se pueden realizar diferentes cosas
 
 Es parte del DOM: Document Object Model
 */
-headline.innerHTML = "Juego del número secreto"
-
-let paragraph = document.querySelector("p");
-paragraph.innerHTML ="elige un número del 1 al 10";
+elementoHTML.innerHTML = texto; 
+}
 
 function userAttempt(){
-    
+    alert("click desde el boton");
 }
+
+asignarTextoElemento("h1", "Juego del número secreto");
+asignarTextoElemento("p", "Indica un número del 1 al 10");
